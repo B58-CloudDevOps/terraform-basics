e6e1e356f56f9f97c01f2a579c7bc395c2ffa4dd
CodingManoj <CodingManoj@gmail.com>
2024-08-26 21:49:30 -0400

    AutoPush

--- 04-functions/output.tf
+++ 04-functions/output.tf
@@ -3,3 +3,7 @@ variable "fruits" {}
 output "apple" {
     value = " Apple color is ${var.fruits["apple"]["color"]} and ${var.fruits["apple"]["metric"]} price is ${var.fruits["apple"]["price"]}"
 }
+
+output "grapes" {
+    value = " Graphes color is ${var.fruits["grapes"]["color"]} and ${var.fruits["grapes"]["metric"]} price is ${var.fruits["grapes"]["price"]}"
+}
