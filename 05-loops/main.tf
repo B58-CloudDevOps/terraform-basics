4aea73e6f1714fa77fb707e66eb6161286eeecfd
CodingManoj <CodingManoj@gmail.com>
2024-08-27 20:48:54 -0400

    AutoPush

--- 05-loops/main.tf
+++ 05-loops/main.tf
@@ -1,12 +1,11 @@
 resource "null_resource" "main" {
-    count = 3                       # count goes by list and each and every iteration is defined by a index number
+  count = 3 # count goes by list and each and every iteration is defined by a index number
 }
 
-
 resource "null_resource" "fruits" {
-    count = length(var.fruits)               
-} 
+  count = length(var.fruits)
+}
 
 variable "fruits" {
-    default = ["grapes" , "orange" , "peer" ]
+  default = ["grapes", "orange", "peer"]
 }
