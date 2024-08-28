77e3a10d09b35c1860c9157dff9fc05a897ce0ce
CodingManoj <CodingManoj@gmail.com>
2024-08-27 20:55:30 -0400

    AutoPush

--- expense-v2/ec2.tf
+++ expense-v2/ec2.tf
@@ -1,10 +1,15 @@
 resource "aws_instance" "main" {
-  count                  = 3
+  count                  = length(var.components)
   ami                    = "ami-0fcc78c828f981df2"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-08c9eb09595f5de07"]
 
   tags = {
-    Name = "frontend-${count.index + 1}"
+    Name = 
   }
 }
+
+variable "components" {
+  default = ["frontend" , "backend" , "mysql"]
+}
+ 
