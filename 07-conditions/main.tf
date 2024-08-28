a85f245ee134f9a06a4a0b4681e5df3dcaa0a0ab
CodingManoj <CodingManoj@gmail.com>
2024-08-27 21:28:19 -0400

    AutoPush

--- 07-conditions/main.tf
+++ 07-conditions/main.tf
@@ -1,6 +1,6 @@
 provider "aws" {}
 
-resource "aws_instance" "main " {
+resource "aws_instance" "main" {
   ami           = "ami-0fcc78c828f981df2"
   instance_type = var.env == dev ? t3.micro : t3.medium
 
