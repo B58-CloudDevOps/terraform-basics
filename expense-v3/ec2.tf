1cd5c3f48c7e9f467f4b4e92c86c2d3592cfcd7c
CodingManoj <CodingManoj@gmail.com>
2024-08-27 21:16:18 -0400

    AutoPush

--- expense-v3/ec2.tf
+++ expense-v3/ec2.tf
@@ -1,21 +1,22 @@
+# Count offers in list and it has it's own cons, so going through for-each will amplify on what we are doing
 resource "aws_instance" "main" {
-  count                  = length(var.components)
+  for_each               = var.components
   ami                    = "ami-0fcc78c828f981df2"
-  instance_type          = 
+  instance_type          = each.value["instance_type"]
   vpc_security_group_ids = ["sg-08c9eb09595f5de07"]
 
   tags = {
-    Name = var.components[count.index]
+    Name = each.key
   }
 }
 
 variable "components" {
   default = {
-    frontend = {                                                      # each.key
+    frontend = { # each.key
       instance_type = "t3.small"
     }
     backend = {
-      instance_type = "t3.micro"                                     # each.value
+      instance_type = "t3.micro" # each.value
     }
     mysql = {
       instance_type = "t2.micro"
