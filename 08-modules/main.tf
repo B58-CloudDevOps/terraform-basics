d8b5767ad1e3f800f59a836b2b3d0c5ae1fd29bc
CodingManoj <CodingManoj@gmail.com>
2024-08-27 22:03:58 -0400

    AutoPush

--- 08-modules/main.tf
+++ 08-modules/main.tf
@@ -1,7 +1,17 @@
 # Calling a backend module which is locally available 
+# Root Module
+
+# An example to demonstrate how to send the information from root module to the backend module while calling.
 module "test" {
   source = "./sample"
+  colors = var.colors # This is how we pass the info to the backend module from the root module
 }
 
 
-# Root Module
+
+
+variable "colors" {
+  default = ["grey", "green"]
+}
+
+### Whatever the variables that you're defining here, should also be defined in the backend as an empty variable
--- 08-modules/sample/main.tf
+++ 08-modules/sample/main.tf
@@ -1,3 +1,10 @@
 # Backend Module 
 
-resource "null_resource" "sample" {}
+resource "null_resource" "sample" {
+  count = length(var.colors)
+}
+
+
+variable "colors" {}
+# Variables that are used in the root module should be mentioned here
+# This is how we are going to receive the information from the module
