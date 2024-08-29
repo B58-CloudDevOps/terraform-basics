6b087abd759b63fd790dfccfe8bb4b3bc70dae00
CodingManoj <CodingManoj@gmail.com>
2024-08-28 21:48:48 -0400

    AutoPush

--- expense-v5/data.tf
+++ expense-v5/data.tf
@@ -5,8 +5,8 @@ data "aws_route53_zone" "main" {
 
 data "aws_security_group" "main" {
   filter {
-    name   = "group-name"     # As per teh documentation : https://docs.aws.amazon.com/AWSEC2/latest/APIReference/API_DescribeSecurityGroups.html
-    values = "b58-allows-all" # Name of your security groups
+    name   = "group-name"       # As per teh documentation : https://docs.aws.amazon.com/AWSEC2/latest/APIReference/API_DescribeSecurityGroups.html
+    values = ["b58-allows-all"] # Name of your security groups
   }
 }
 
