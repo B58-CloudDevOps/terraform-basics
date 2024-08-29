1e1cfec72771e93d42bf594fce2ec809da61829d
CodingManoj <CodingManoj@gmail.com>
2024-08-28 21:12:34 -0400

    AutoPush

--- expense-v5/app/main.tf
+++ expense-v5/app/main.tf
@@ -1,3 +1,4 @@
+# Creates EC2 Instance 
 resource "aws_instance" "main" {
   ami                    = var.ami
   instance_type          = var.instance_type
@@ -7,3 +8,14 @@ resource "aws_instance" "main" {
     Name = var.name
   }
 }
+
+# Creates DNS Record
+resource "aws_route53_record" "main" {
+  zone_id = var.zone_id
+  name    = "${var.name}-dev.expense.internal"
+  type    = "A"
+  ttl     = 10
+  records = [aws_instance.main.private_ip]
+}
+
+
